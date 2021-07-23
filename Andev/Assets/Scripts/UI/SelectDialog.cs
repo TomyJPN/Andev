cdc319714615306f8672ed7268be31d8a168d3a2
DESKTOP-GALLERIA\TOMY <masaya120331@gmail.com>
2021-07-23 19:17:39 +0900

    UIマネージャーとダイアログの作成

---
+++ Andev/Assets/Scripts/UI/SelectDialog.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectDialog : MonoBehaviour
+{
+    [Serializable]
+    public class ButtonTextPair
+    {
+        public Text Text;
+        public Button Button;
+    }
+
+    [SerializeField]
+    private List<ButtonTextPair> buttonTextList;
+
+    public void Setup(List<UIManager.ButtonRegisterData> buttonRegisterDataList)
+    {
+        HideButtons();
+
+        int i = 0;
+        foreach (var data in buttonRegisterDataList)
+        {
+            if (i > buttonTextList.Count)
+            {
+                Debug.LogError($"SelectDialog > ボタン登録に想定外の数量のデータです:{i}");
+                break;
+            }
+
+            buttonTextList[i].Text.text = data.ButtonText;
+            buttonTextList[i].Button.onClick.AddListener(() =>
+            {
+                data.ButtonCallback?.Invoke();
+                OnClickButton();
+            }
+            );
+
+            buttonTextList[i].Button.gameObject.SetActive(true);
+
+            i++;
+        }
+    }
+
+    private void HideButtons()
+    {
+        foreach(var button in buttonTextList)
+        {
+            button.Button.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnClickButton()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Andev/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class UIManager : SingletonMonoBehaviour<UIManager>
+{
+    private GameObject _canvasObj;        
+
+    /// <summary>
+    /// ボタンの登録に使うデータ
+    /// </summary>
+    public class ButtonRegisterData
+    {
+        public string ButtonText;
+        public Action ButtonCallback;
+    }
+
+    protected override void OnInitialize()
+    {
+        _canvasObj = GameObject.Find("Canvas");
+        if(_canvasObj == null)
+        {
+            Debug.LogError("Canvasを取得できませんでした");
+        }
+    }
+
+    /// <summary>
+    /// 選択肢ダイアログの表示
+    /// </summary>
+    public void ShowSelectDialog(List<ButtonRegisterData> buttonRegisterDataList)
+    {
+        GameObject prefab = (GameObject)Resources.Load("SelectDialog");
+
+        if(prefab == null)
+        {
+            Debug.LogError("ShowSelectDialog > ロード失敗");
+            return;
+        }
+
+        GameObject go = Instantiate(prefab);
+        go.GetComponent<SelectDialog>().Setup(buttonRegisterDataList);
+        go.transform.SetParent(_canvasObj.transform, false);
+    }
+}
