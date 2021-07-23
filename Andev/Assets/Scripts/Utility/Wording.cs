4070488e2d2ace2dd2fd4274521b15991ac162fe
DESKTOP-GALLERIA\TOMY <masaya120331@gmail.com>
2021-07-23 19:18:01 +0900

    文言の管理作成

---
+++ Andev/Assets/Scripts/Utility/Wording.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wording
+{
+    private static readonly Dictionary<string, string> _wordMap = new Dictionary<string, string>
+    {
+        { "bet", "賭ける" },
+        { "call", "勝負する" },
+        { "pass", "パスする" },
+        { "raise", "ふやす" },
+        { "fold", "おりる" },
+        { "", "" },
+    };
+
+    /// <summary>
+    /// 文言のロード
+    /// </summary>
+    public static string LoadWord(string wordingKey)
+    {
+        string key = wordingKey.ToLower();
+
+        if (!_wordMap.ContainsKey(key))
+        {
+            Debug.LogError($"LoadWord > 文言:{key}がありません");
+            return "";
+        }
+
+        return _wordMap[key];
+    }
+}
